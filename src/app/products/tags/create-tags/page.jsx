536f54ed5fc6ee84ae7df96c0da1ceab5d03ec90
israt <israt9528@gmail.com>
2026-01-31 20:44:07 +0600

    fix error

--- src/app/products/tags/create-tags/page.jsx
+++ src/app/products/tags/create-tags/page.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-=======
-
->>>>>>> 8cfb7684a83f8743fe64917c49d546a2e4622d1b
 import TabbedLayout from "@/Components/Layout/TabbedLayout";
 import TagsGeneralForm from "@/Components/Forms/Tags/TagsGeneralForm";
 
